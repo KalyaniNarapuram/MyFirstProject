463440c2d74f45e9879cd17895a8a32177eac4e3
KalyaniNarapuram <kattakalyani@gmail.com>
2019-02-28 20:11:06 +0000

    myfirstcommit

---
+++ src/love/programming/MyMainClass.java
@@ -0,0 +1,17 @@
+package love.programming;
+
+public class MyMainClass {
+    public static void main(String[] args) {
+        System.out.println("Namasthe");
+
+        int a = 5;
+        int b = 10;
+        int z = a+b;
+                System.out.println(z);
+
+        String x = "Kalyani";
+        String y = "Narapuram";
+        String w = x+y;
+                System.out.println(w);
+    }
+}
